06751db444728ff88408e5a790363373de7e76e3
Jack <jackeagle102@gmail.com>
2014-11-18 09:30:16 +0530

    Added I9082 Device

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,1 @@
-for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-11.0 | awk {'print $1'})
-do
-    add_lunch_combo $combo
-done
+add_lunch_combo aogp_i9082-user
