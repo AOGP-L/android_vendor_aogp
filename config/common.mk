9c932d14513d07d88c17ab2b77502825f30decd0
Janson Kang <temasek71@gmail.com>
2014-12-01 10:38:36 +0800

    Revert "vendor/omni: Fix-ups for prebuilt Chromium"
    This reverts commit 6ea61a3aeb81f98567a6cc3a156761d9c2cc4bb8.

--- config/common.mk
+++ config/common.mk
@@ -130,11 +130,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
 
-# Chromium Prebuilt
-ifeq ($(PRODUCT_PREBUILT_WEBVIEWCHROMIUM),yes)
--include prebuilts/chromium/$(TARGET_DEVICE)/chromium_prebuilt.mk
-endif
-
 # This is CM!
 PRODUCT_COPY_FILES += \
     vendor/cm/config/permissions/com.cyanogenmod.android.xml:system/etc/permissions/com.cyanogenmod.android.xml
