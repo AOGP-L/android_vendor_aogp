e4037dd809831a85389772fbf1d18441509527e4
Steve Kondik <steve@cyngn.com>
2014-12-09 13:29:29 -0800

    cm: Enable AudioFX
     * Drop DSPManager and libcyanogen-dsp. Devices should switch to
       standard Android effects or offload-capable versions.
    
    Change-Id: I4f4eec5747e9073e576fe70e720ac3ffebb3fdd9
    (cherry picked from commit c4f6b977c5b320b7ccef98f5ba9fa716ceacfd82)
    
    Conflicts:
    	config/common.mk

--- config/common.mk
+++ config/common.mk
@@ -186,9 +186,7 @@ PRODUCT_PACKAGES += \
 # Custom CM packages
 PRODUCT_PACKAGES += \
     Trebuchet \
-    DSPManager \
-    libcyanogen-dsp \
-    audio_effects.conf \
+    AudioFX \
     CMFileManager \
     Eleven \
     LockClock \
