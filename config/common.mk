LiquidSmooth-Old/android_vendor_liquid

vendor: Add latinime lib needed for keyboard swyping
* Not all gapps contain this so let's ensure our users can swype.

Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
Change-Id: I93a17954cdd79ee802ef23df0307a71ade72d7a6

config/common.mk
@@ -130,6 +130,10 @@ PRODUCT_PACKAGES += \
     LatinIME \
     BluetoothExt
 
+# Proprietary latinime lib needed for Keyboard swyping
+PRODUCT_COPY_FILES += \
+    vendor/liquid/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+
 # Viper4Android
 PRODUCT_COPY_FILES += \
     vendor/liquid/prebuilt/common/etc/viper4android/viper4android.apk:system/app/Viper4Android/viper4android.apk

AOGP-L/android_vendor_aogp

Conflicts:
	config/common.mk

config/common.mk
@@ -128,6 +128,10 @@ PRODUCT_PACKAGES += \
     Basic \
     libemoji
 
+# Proprietary latinime lib needed for Keyboard swyping
+PRODUCT_COPY_FILES += \
+    vendor/aogp/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+
 # Custom AOGP packages
 PRODUCT_PACKAGES += \
     Launcher3 \