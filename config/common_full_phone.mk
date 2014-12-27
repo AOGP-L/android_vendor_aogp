6a0c789913dc685a40d1fc47c5734819bf7e4337
kar <kar2wlan@gmail.com>
2014-12-27 16:08:07 +0530

    delete remains of bootanimation copying.
    Causing error
    Copy: /home/jackeagle/aogpl/out/target/product/i9082/system/media/audio/ringtones/Sheep.mp3
    make: *** No rule to make target 'vendor/aogp/prebuilt/common/bootanimation/480.zip', needed by '/home/jackeagle/aogpl/out/target/product/i9082/system/media/bootanimation.zip'. Stop.
    make: *** Waiting for unfinished jobs....
    jackeagle@aogp:~/aogpl$

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -6,9 +6,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=Argon.ogg \
     ro.config.alarm_alert=Hassium.ogg
 
-ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-    PRODUCT_COPY_FILES += \
-        vendor/aogp/prebuilt/common/bootanimation/480.zip:system/media/bootanimation.zip
-endif
-
 $(call inherit-product, vendor/aogp/config/telephony.mk)
